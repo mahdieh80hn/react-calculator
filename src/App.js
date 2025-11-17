61772edeccff4e4ca38f2e7597a9850d131f807b
mahdieh80hn <sm.hatamnezhad@gmail.com>
2025-11-17 16:32:07 +0330

    Add functionallity to operators and other buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,86 @@
-const buttons = [
-  "AC", "DEL", "%", "/",
-  "7", "8", "9", "*",
-  "4", "5", "6", "-",
-  "1", "2", "3", "+",
-  "0", ".", "="
-];
+import { cloneElement, useState } from "react";
 
-export default function App() {
+
+
+function App() {
+
+  const [currentInput, setCurrentInput] = useState("")
+  const [previousInput, setPreviousInput] = useState("")
+  const [operator, setOperator] = useState(null)
+
+  const buttons = [
+    "C", "/", "*", "Del",
+    "7", "8", "9", "-",
+    "4", "5", "6", "+",
+    "1", "2", "3", "=",
+    "0", ".",
+  ];
+
+  const handleClick = (value) => {
+    if (value === "C") {
+      setCurrentInput("")
+      setPreviousInput("")
+      setOperator(null)
+      return
+    }
+
+    if (value === "Del") {
+      setCurrentInput(currentInput.slice(0, -1))
+      return
+    }
+
+    if(!isNaN(value) || value === "."){
+      setCurrentInput(currentInput + value)
+      return
+    }
+
+    if (["+", "-", "*", "/"].includes(value)) {
+      if (currentInput === "") return
+
+      setOperator(value)
+      setPreviousInput(currentInput)
+      setCurrentInput("")
+      return
+    }
+
+
+    // calculation
+    if(value === "="){
+      if(!operator || currentInput === "") return
+
+      const prev = parseFloat(previousInput)
+      const curr = parseFloat(currentInput)
+
+      let result = 0
+
+      switch (operator){
+        case "+":
+          result = prev + curr
+          break;
+        case "-":
+          result = prev - curr
+          break;
+        case "*":
+          result = prev * curr
+          break;
+        case "/":
+          result = prev / curr
+          break;
+        default:
+          break;
+      }
+      setCurrentInput(String(result))
+      setPreviousInput("")
+      setOperator(null)
+    }
+  }
   return (
     <div className="w-full h-screen bg-gray-900 flex items-center justify-center">
       <div className="bg-gray-800 p-4 rounded-xl w-80">
-        
+
         {/* Display */}
         <div className="bg-black text-white text-right text-3xl p-4 rounded-lg mb-4 h-16 flex items-center justify-end">
-          0
+          {currentInput || previousInput || "0"}
         </div>
 
         {/* Buttons */}
@@ -21,6 +88,7 @@ export default function App() {
           {buttons.map((btn, index) => (
             <button
               key={index}
+              onClick={() => handleClick(btn)}
               className="bg-gray-700 hover:bg-gray-600 active:bg-gray-500 transition p-4 text-xl rounded-lg text-white"
             >
               {btn}
@@ -31,3 +99,4 @@ export default function App() {
     </div>
   );
 }
+export default App;
